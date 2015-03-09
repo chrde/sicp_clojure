046aaee137f7b98ea7bd5666c4411a37e235d49d
Christian de la Hoz <apigolas@gmail.com>
2015-03-09 12:40:43 +0100

    Exercise 2.7 & 2.8 finished

--- src/chapter2/part1/exercises.clj
+++ src/chapter2/part1/exercises.clj
@@ -112,3 +112,38 @@
   (fn [f]
     (fn [x]
       ((b (a f)) x))))
+
+;; 2.7
+(defn make-interval [lower-bound upper-bound]
+  (conss lower-bound upper-bound))
+
+(defn lower-bound [interval]
+  (min (car interval)
+       (cdr interval)))
+
+(defn upper-bound [interval]
+  (max (car interval)
+       (cdr interval)))
+
+(defn add-interval [x y]
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(defn mul-interval [x y]
+  (let [p1 (* (lower-bound x) (lower-bound y))
+        p2 (* (lower-bound x) (upper-bound y))
+        p3 (* (upper-bound x) (upper-bound y))
+        p4 (* (upper-bound x) (lower-bound y))]
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(defn div-interval [x y]
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+;; 2.8
+(defn sub-interval [x y]
+  (add-interval x
+                (make-interval (- (lower-bound y))
+                               (- (upper-bound y)))))
