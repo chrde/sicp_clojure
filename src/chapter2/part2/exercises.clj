024100177595be39f7a980b207aad330de091d7a
Christian de la Hoz <apigolas@gmail.com>
2015-03-11 14:43:42 +0100

    Exercises 2.17-2.19 finished

---
+++ src/chapter2/part2/exercises.clj
@@ -0,0 +1,33 @@
+(ns chapter2.part2.exercises
+  (require [chapter2.part2.samples :as ch2]))
+
+;; 2.17
+(defn last-pair [l]
+  (if (nil? (ch2/cdr l))
+    (ch2/car l)
+    (last-pair (ch2/cdr l))))
+
+;; 2.18
+(defn reverse
+  ([l] (reverse l (empty l)))
+  ([l r-l]
+    (if (nil? (ch2/car l))
+      r-l
+      (reverse (ch2/cdr l) (cons (ch2/car l) r-l)))))
+
+;; 2.19
+(defn no-more? [l]
+  (zero? (ch2/length l)))
+
+
+(defn except-first-denomination [coin-values]
+  (ch2/cdr coin-values))
+
+(defn first-denomination [coin-values]
+  (ch2/car coin-values))
+
+(defn count-change [amount coin-values]
+  (cond (zero? amount) 1
+        (or (< amount 0) (no-more? coin-values)) 0
+        :else (+ (count-change amount (except-first-denomination coin-values))
+                   (count-change (- amount (first-denomination coin-values)) coin-values))))
---
+++ src/chapter2/part2/samples.clj
@@ -0,0 +1,23 @@
+(ns chapter2.part2.samples)
+
+(defn car [l]
+  (first l))
+
+(defn cdr [l]
+  (next l))
+
+(defn list-ref [l n]
+  (if (zero? n)
+    (car l)
+    (list-ref (cdr l) (dec n))))
+
+(defn length [l]
+  (if (nil? l)
+    0
+    (inc (length (cdr l)))))
+
+(defn append [l1 l2]
+  (if (nil? l1)
+    l2
+    (cons (car l1) (append (cdr l1) l2))
+    ))
