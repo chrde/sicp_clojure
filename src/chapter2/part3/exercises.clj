c4f5b2b96b8b8d29c2b97b991b57ea751bd09e93
Christian de la Hoz <apigolas@gmail.com>
2015-03-19 10:18:43 +0100

    Exercise 2.56

--- src/chapter2/part3/exercises.clj
+++ src/chapter2/part3/exercises.clj
@@ -1,5 +1,7 @@
 (ns chapter2.part3.exercises
-  (:require [chapter2.part2.samples :as ch2]))
+  (:require [chapter2.part2.samples :as ch2]
+            [chapter2.part3.samples :refer :all]
+            [chapter1.samples :as ch1]))
 
 ;; 2.54
 (defn equal? [x y]
@@ -7,3 +9,42 @@
     (and (equal? (ch2/car x) (ch2/car y))
          (equal? (ch2/cdr x) (ch2/cdr y)))
     (= x y)))
+
+;; 2.56
+(defn base [s]
+  (ch2/cadr s))
+
+(defn exponent [s]
+  (ch2/caddr s))
+
+(defn make-exponentiation [base exponent]
+  (list '** base exponent))
+
+(defn make-exponentiation-simplest [base exponent]
+  (cond (=number? exponent 0) 1
+        (=number? exponent 1) base
+        (and (number? base) (number? exponent)) (ch1/recur-fast-expt base exponent)
+        :else (make-exponentiation base exponent)))
+
+(defn exponentiation? [x]
+  (and (coll? x) (eq? (ch2/car x) '**)))
+
+(defn deriv-exp [exp var]
+  (cond (number? exp) 0
+        (variable? exp) (if (same-variable? exp var) 1 0)
+        (sum? exp)
+        (make-sum-simplest (deriv-exp (addend exp) var)
+                           (deriv-exp (augend exp) var))
+        (product? exp)
+        (make-sum-simplest (make-product-simplest (multiplier exp)
+                                                  (deriv-exp (multiplicand exp) var))
+                           (make-product-simplest (deriv-exp (multiplier exp) var)
+                                                  (multiplicand exp)))
+        (exponentiation? exp)
+        (make-product-simplest (make-product-simplest (exponent exp)
+                                                      (make-exponentiation-simplest
+                                                        (base exp)
+                                                        (make-sum-simplest (exponent exp) -1)))
+                               (deriv-exp (base exp) var))
+
+        :else (throw (Exception. (str "Unknown expression type: DERIV-EXP" exp)))))
--- test/chapter2/part3/exercises_test.clj
+++ test/chapter2/part3/exercises_test.clj
@@ -8,4 +8,24 @@
     (is (equal? '(this is a list) '(this is a list)))
     (is (not (equal? '(this is a list) '(this (is a) list))))))
 
+(deftest deriv-exp-test
+  (testing "2.56 - exponentiation selectors"
+    (is (= 4 (base (make-exponentiation 4 5)))
+        "base selector")
+    (is (= 5 (exponent (make-exponentiation 4 5)))
+        "exponent selector"))
+  (testing "2.56 - simplify exponentiation"
+    (is (= 1 (make-exponentiation-simplest 'x 0)))
+    (is (= 'x (make-exponentiation-simplest 'x 1))))
+  (testing "2.56 - deriv with exponentiation"
+    (is (= '(* n (** x (+ n -1)))
+           (deriv-exp '(** x n) 'x)))
+    (is (= '(+ (* a (* 2 x)) b)
+           (deriv-exp '(+ (+ (* a (** x 2)) (* b x)) c) 'x))
+        "first deriv. of ax^2 + bx + c = 2ax + b")
+    (is (= '(* a 2) (deriv-exp (deriv-exp '(+ (+ (* a (** x 2)) (* b x)) c) 'x) 'x))
+        "second deriv. of ax^2 + bx +c = 2a")
+    (is (= 0 (deriv-exp (deriv-exp (deriv-exp '(+ (+ (* a (** x 2)) (* b x)) c) 'x) 'x) 'x))
+        "third deriv. of ax^2 + bx +c = 0")))
+
 (run-tests)
