b691b514676d3bdf4e5b02a6a9223979fd43bc8f
Christian de la Hoz <apigolas@gmail.com>
2015-03-30 14:34:42 +0200

    Exercise 2.68

--- src/main/chapter2/part3/huffman_trees.clj
+++ src/main/chapter2/part3/huffman_trees.clj
@@ -1,5 +1,7 @@
 (ns chapter2.part3.huffman-trees
-  (:require [chapter2.part3.huffman-trees-samples :refer :all]))
+  (:require [chapter2.part3.huffman-trees-samples :refer :all]
+            [chapter2.part2.samples :refer [car cdr cadr caddr append] :as ch2]
+            [chapter2.part3.representing-sets-samples :refer [element-of-set?]]))
 
 ;; 2.67
 (def sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
@@ -9,3 +11,24 @@
                   (make-code-tree (make-leaf 'B 2)
                                   (make-code-tree (make-leaf 'D 1)
                                                   (make-leaf 'C 1)))))
+
+;; 2.68
+(defn contains-symbol? [symbol branch]
+  (if (leaf? branch)
+    (eq? symbol (symbol-leaf branch))
+    (element-of-set? symbol (symbols branch))))
+
+(defn encode-symbol [symbol tree]
+  (if (leaf? tree)
+    '()
+    (let [right (right-branch tree)
+          left (left-branch tree)]
+      (cond (contains-symbol? symbol right) (cons 1 (encode-symbol symbol right))
+            (contains-symbol? symbol left) (cons 0 (encode-symbol symbol left))
+            :else (throw (Exception. (str "bad symbol: ENCODE SYMBOL" symbol)))))))
+
+(defn encode [message tree]
+  (if (nil? message)
+    '()
+    (ch2/append (encode-symbol (car message) tree)
+                (encode (cdr message) tree))))
--- test/chapter2/part3/huffman_trees_test.clj
+++ test/chapter2/part3/huffman_trees_test.clj
@@ -4,6 +4,11 @@
             [chapter2.part3.huffman-trees-samples :as ch3]))
 
 (deftest huffman-decode-test
-  (testing "2.67 - Decoding a huffman tree"
+  (testing "2.67 - Decoding with huffman"
     (is (= '(A D A B B C A)
            (ch3/decode sample-message sample-tree)))))
+
+(deftest huffman-encode-test
+  (testing "2.68 - Encoding with huffman"
+    (is (= sample-message
+           (encode (ch3/decode sample-message sample-tree) sample-tree)))))
