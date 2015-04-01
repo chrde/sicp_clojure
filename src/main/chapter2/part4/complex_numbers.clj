9f2d1019bdc6d57d6ce2eeba6d1e2f61e350f146
Christian de la Hoz <apigolas@gmail.com>
2015-04-01 12:24:03 +0200

    TODO - exercises 2.73 - 2.84

--- src/main/chapter2/part4/complex_numbers.clj
+++ src/main/chapter2/part4/complex_numbers.clj
@@ -1,1 +1,20 @@
 (ns chapter2.part4.complex-numbers)
+
+;; TODO - after creating table in chapter 3, and defining put and get Operations
+;; 2.73
+;; 2.74
+;; 2.75
+;; 2.76
+; When something changes, it would be nice to make changes in just one place
+; With this in mind, I would say
+; When new types are often added, message-passing-style is better
+; - Because the entire type definition can be seen as one module in the source code
+; When new operations are often added, data-directed-style is better
+; - Because the operation (for all the types) can be kept in one module as a whole
+
+; These are reasons from a developing/maintaining point of view, since ultimately both
+; options do "the same"
+
+;; 2.78
+;; 2.79
+;; 2.80
